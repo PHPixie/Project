ff594239417cb0abc37b34a52aa794c86759c302
Dracony <draconyster@gmail.com>
2013-02-05 19:54:21 +0200

    tests for controller and view

---
+++ tests/system/mocks/misc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Mock class for Misc
+ */
+class Misc{
+	public static $file;
+	public static function find_file(){
+		return static::$file;
+	}
+}
---
+++ tests/system/mocks/testController.php
@@ -0,0 +1,13 @@
+<?php
+class Test_Controller extends Controller {
+	public $counter = 0;
+	public function before() {
+		$this->counter++;
+	}
+	public function after() {
+		$this->counter++;
+	}
+	public function action_index() {
+		$this->counter++;
+	}
+}
